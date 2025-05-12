25a465f436cd6f99a1cc031e28f7b2299ce69e86
Adeke Msonter Samuel <128712012+adekemsontersamuel@users.noreply.github.com>
2025-05-12 15:21:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -127,6 +127,7 @@ app.post('/send-message', async (req, res) => {
         });
     }
 });
+ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
